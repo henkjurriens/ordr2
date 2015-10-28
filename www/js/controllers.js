22794277c963d9107a219733dce557247b472c60
Henk Jurriens <henk.jurriens@gmail.com>
2015-10-28 13:41:08 +0100

    proxy toegevoegd

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,15 +4,35 @@ angular.module('starter.controllers', ['ui.router'])
   $scope.items = Menu.all();
 })
 
-.controller('OrderCtrl',  function($scope, $state) {
+.controller('OrderCtrl',  function($scope, $state, $http) {
 
   $scope.sandwich = '';
   $scope.setSandwich = function(sandwich) {
     $scope.sandwich = sandwich;
     console.log('sandwich : ' + sandwich);
     localStorage.setItem("sandwich", sandwich);
+    $scope.send();
+
     $state.go('tab.order-spreads')
   }
+
+
+  $scope.send = function() {
+    console.log('send');
+    var data = { 'naam' : 'henk',
+                 'broodje': 'pistolet',
+                  'beleg': 'Filet americain',
+                  'bestelDatum': '28-10-2015T10:30:00'
+                }
+
+    console.log('data :', data.naam);
+
+      $http.post('http://localhost:8100/api/broodjes', data, {}).then(
+       function() {},
+       function() {}
+      );
+  }
+
 
 })
 
